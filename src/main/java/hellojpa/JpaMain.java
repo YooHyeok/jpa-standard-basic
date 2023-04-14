c7bde4e32871cd90e1dacb2dbe4db891fcdc3b31
YooHyeok <yjk7454@naver.com>
2023-04-15 01:36:14 +0900

    트랜잭션 관리 코드 추가  및 Entity옵션 추가 주석 설명

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,14 +2,34 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
+        // Entity Manager Factory는 애플리케이션 로딩 시점에 딱 하나만 만들어야한다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
+
+        //트랜잭션 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
         //code
+        // 실제 DB에 저장하는 트랜잭션 단위, 예를 들어 고객의 행위를 하고 나갈때(상품장바구니추가등) 할때마다
+        // DB Connection을 얻어 Query를 날리고 종료되는 한 일괄적인 단위의 활동을 할때 만들어줘야한다.
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("HelloA");
+
+        em.persist(member); // Member가 저장된다.
+        //JPA에서는 트랜잭션이라는 처리가 중요하다.
+        // 모든 데이터를 변경하는 모든 작업은 JPA에서 꼭 Transaction 안에서 작업을 해야한다. (16~17라인 추가)
+        tx.commit();
+
+
         em.close();
 
         emf.close();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,12 +1,16 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity //JPA 첫로딩시 Entity로 관리할수 있도록 인식
+//@Table(name = "USER") // DB에 Member라는 이름이 아닌 User라는 이름의 테이블을 설정한다면 해당 어노테이션으로 지정한다. 생략시 엔티티 클래스명으로 테이블이 관리됨.
 public class Member {
     @Id
     private Long id;
+//    @Column(name = "username") //DB 컬럼이 username이라면 해당 어노테이션을 통해서 Entity가 관리된다. (생략시 필드명으로 컬럼명이 관리됨)
     private String name;
 
     public Long getId() {
