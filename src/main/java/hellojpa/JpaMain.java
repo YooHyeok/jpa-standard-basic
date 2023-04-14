0f502a28623ee947f7d47e2671a20be1913abe7d
YooHyeok <yjk7454@naver.com>
2023-04-15 01:40:15 +0900

    Try Catch 안에서 트랜잭션 관련 에러처리 - 정석코드

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,18 +20,20 @@ public class JpaMain {
         // 실제 DB에 저장하는 트랜잭션 단위, 예를 들어 고객의 행위를 하고 나갈때(상품장바구니추가등) 할때마다
         // DB Connection을 얻어 Query를 날리고 종료되는 한 일괄적인 단위의 활동을 할때 만들어줘야한다.
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
-
-        em.persist(member); // Member가 저장된다.
-        //JPA에서는 트랜잭션이라는 처리가 중요하다.
-        // 모든 데이터를 변경하는 모든 작업은 JPA에서 꼭 Transaction 안에서 작업을 해야한다. (16~17라인 추가)
-        tx.commit();
-
-
-        em.close();
-
-        emf.close();
+        //Try Catch 안에서 트랜잭션의 예외를 처리해야 한다.
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member); // Member가 저장된다.
+            //JPA에서는 트랜잭션이라는 처리가 중요하다.
+            // 모든 데이터를 변경하는 모든 작업은 JPA에서 꼭 Transaction 안에서 작업을 해야한다. (16~17라인 추가)
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback(); // 데이터 저장시 문제 발생하면 트랜잭션을 롤백한다.
+        } finally {
+            em.close(); // 엔티티매니저 닫기.(내부적으로 DB 커넥션을 물고 동작하므로 사용후에는 꼭 닫아줘야한다)
+        }
+        emf.close(); //전체 애플리케이션이 종료되면 EntityManagerFactory도 닫아줘야한다.
     }
 }
